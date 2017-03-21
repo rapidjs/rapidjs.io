3cd35a31bde2b03c54dde4f125ea1b57c7ad6c1a
Drew Bartlett <drewb9@gmail.com>
2017-03-21 11:35:50 -0700

    latest

--- resources/views/documentation/relationships/master.blade.php
+++ resources/views/documentation/relationships/master.blade.php
@@ -1,3 +1,8 @@
 <div class="docs__group">
     <h1 id="relationships">Relationships</h1>
+
+    <div class="docs__section">
+        <p>Firstly, I am no relationship expert-that's for sure. (Writing docs takes a while...have to make it fun, right?). Anyway, </p>
+        
+    </div>
 </div>

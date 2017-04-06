df3f2e6c319cac31d8abfc588f65e391674f2808
Drew Bartlett <drewb9@gmail.com>
2017-04-06 08:45:03 -0700

    some changes to the site

--- config/docs.php
+++ config/docs.php
@@ -5,5 +5,7 @@ return [
 
     'extending-rapid' => ['base-models' => 'Base Models', 'auth-model' => 'Built In Auth', 'making-a-wrapper' => 'Making a Wrapper'],
 
-    'debugging' => ['testing-routes' => 'Testing Routes', 'logging' => 'Logging']
+    'debugging' => ['testing-routes' => 'Testing Routes', 'logging' => 'Logging'],
+
+    'pages' => ['getting-started.master', 'extending-rapid.master', 'config-builder.app', 'methods.master', 'configuration.master']
 ];
--- resources/views/pages/documentation.blade.php
+++ resources/views/pages/documentation.blade.php
@@ -8,20 +8,10 @@
 
         <div class="docs fb-grid col-xs-12 col-md-9 col-md-offset-2">
 
-            @include('documentation.getting-started.master')
-
-            {{-- @include('documentation.relationships.master') --}}
-
-            @include('documentation.extending-rapid.master')
-
-            @include('documentation.class-builder.app')
-
-            {{-- @include('documentation.debugging.master') --}}
-
-            @foreach(['methods', 'configuration'] as $file)
-                <div class="docs__section">
-                    @include('documentation.'.$file.'.master')
-                </div>
+            @foreach(config('docs')['pages'] as $page)
+                @if(View::exists('documentation.' . $page))
+                    @include('documentation.'. $page)
+                @endif
             @endforeach
 
             @include('layouts.footer')
